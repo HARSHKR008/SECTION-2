1f7b9a502d505ed53e1a0962b130f41b68ba6a10
HARSHKR008 <i.harshk0008@gmail.com>
2025-03-17 12:41:15 +0530

    new jsx files

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className='font-bold text-center text-3xl'>
+        My home page</h1>
+    </div>
+  )
+}
+
+export default Home
